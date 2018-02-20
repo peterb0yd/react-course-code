9efa6ee48960b6070f7385fba86b284f03e20c77
Peter Boyd <peterboyd192@gmail.com>
2018-02-19 22:03:36 -0500

    update

--- http-project/src/containers/Blog/Blog.js
+++ http-project/src/containers/Blog/Blog.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import axios from 'axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+import './Blog.css'
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get('/posts')
+      const posts = res.data.slice(0, 4)
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: 'Peter'
+        }
+      })
+      this.setState({ posts: updatedPosts })
+    } catch (e) {
+      console.error(e)
+      this.setState({ error: true })
     }
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id })
+  }
+
+  render() {
+    let posts = <p style={{textAlign: 'center', color: 'red'}}>Something went wrong!</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post
+          key={post.id}
+          author={post.author}
+          title={post.title}
+          clicked={() => this.postSelectedHandler(post.id)} />
+      })
+    }
+
+    return (<div>
+      <section className="Posts">
+        {posts}}
+      </section>
+      <section>
+        <FullPost
+          id={this.state.selectedPostId}/>
+      </section>
+      <section>
+        <NewPost/>
+      </section>
+    </div>);
+  }
 }
 
 export default Blog;
